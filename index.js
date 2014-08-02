65ce741daccbcc76e4c9270c56c2f1b7c1f23a52
yukimono <gr33nmous3@gmail.com>
2014-08-02 11:25:52 +0530

    openshift fix

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ var express = require("express"),
 app.engine("html", thunder);
 app.set("view engine", "html");
 app.set("views", "./view");
+app.set("domain", process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0");
+app.set("port", process.env.OPENSHIFT_NODE_PORT || process.env.PORT || 8080);
 
 var db = null;
 
@@ -17,4 +19,4 @@ app.use(static("./pub"));
 
 require("./route/index.js")(app, db);
 
-app.listen(process.env.VMC_PORT || process.env.PORT || 8080);
+app.listen();
