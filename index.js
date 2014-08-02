769acfaa8d16130912dd80b8bf151ae02e9ac8cf
yukimono <gr33nmous3@gmail.com>
2014-08-02 11:38:45 +0530

    Now literally copying code

--- index.js
+++ index.js
@@ -16,4 +16,4 @@ app.use(static("./pub"));
 
 require("./route/index.js")(app, db);
 
-app.listen(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080, process.env.OPENSHIFT_NODEJS_PORT || "localhost");
+app.listen(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || process.env.VMC_PORT || 8080, process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0", function() { console.log("started"); });
