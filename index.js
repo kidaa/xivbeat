a66fe97866eaf7e50a9f056fd02fd51d333f805c
yukimono <gr33nmous3@gmail.com>
2014-08-02 11:33:08 +0530

    last try on openshift

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ var express = require("express"),
 app.engine("html", thunder);
 app.set("view engine", "html");
 app.set("views", "./view");
-app.set("domain", process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0");
-app.set("port", process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080);
-
 var db = null;
 
 app.use(require("./route/api")(db));
@@ -19,4 +16,4 @@ app.use(static("./pub"));
 
 require("./route/index.js")(app, db);
 
-app.listen();
+app.listen(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080, process.env.OPENSHIFT_NODEJS_PORT || "localhost");
