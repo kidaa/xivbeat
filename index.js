ca3840c5c72da73d431adaaad102e2c0f62ad6f4
yukimono <gr33nmous3@gmail.com>
2014-09-20 21:24:38 +0530

    CLI Switches for specific subdomains

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 //Copyright 2014 Yuki Ahmed
 
 var express = require("express"),
-    static = require("serve-static"),
     thunder = require("express-thunder"),
     subdomain = require("express-subdomain"),
     app = express();
@@ -12,10 +11,37 @@ app.set("view engine", "html");
 app.set("views", "./view");
 var db = null;
 
-require("./subdomain/api/")(app, db, subdomain, express.Router());
-require("./subdomain/time/")(app, db, subdomain, express.Router());
-require("./subdomain/pf/")(app, db, subdomain, express.Router());
-require("./route/index.js")(app, db);
-app.use(static("./pub"));
+var params = {api: true, time: true, pf: true, main: true};
+
+if(process.argv.length > 2) {
+  for(var key in params) {
+    params[key] = false;
+  }
+}
+
+for(var i = 1; i < process.argv.length; ++i) {
+  var arg = process.argv[i];
+
+  for(var key in params) {
+    var l = key[0];
+    if(arg == "--" + key || arg == "-" + l) {
+      params[key] = true;
+      console.log(key, "enabled");
+    }
+  }
+}
+
+if(params.api === true) {
+  require("./subdomain/api/")(app, db, subdomain, express.Router());
+}
+if(params.time === true) {
+  require("./subdomain/time/")(app, db, subdomain, express.Router());
+}
+if(params.pf === true) {
+  require("./subdomain/pf/")(app, db, subdomain, express.Router());
+}
+if(params.main === true) {
+  require("./route/index.js")(app, db, express.Router());
+}
 
 app.listen(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || process.env.VMC_PORT || 8080, process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0", function() { console.log("started"); });
--- route/index.js
+++ route/index.js
@@ -1,8 +1,15 @@
 //Licensed under the ISC license.
 //Copyright 2014 Yuki Ahmed
 
-exports = module.exports = function(app, db) {
-  app.get("/world/:datacenter/:world", function(req, res) {
+var static = require("serve-static"),
+    path = require("path");
+
+exports = module.exports = function(app, db, router) {
+  router.use(static(path.resolve(__dirname, "..", "pub")));
+
+  router.get("/world/:datacenter/:world", function(req, res) {
     res.render("tracking.html", {});
   });
+
+  app.use(router);
 }
